1fda8c3f0db7b22bfb35b8f07eead70e85771dfe
jp-soaresr <soaresr0105@gmail.com>
2024-08-20 19:29:40 -0300

    Aula1

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt">
+<cabeça>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="largura=largura-do-dispositivo, escala-inicial=1.0">
+    <title>Documento</title>
+</cabeçalho>
+<corpo>
+    <h1>hoje é dia <?php  echo  date ( " d/m/Y " ); ?> </h1>
+    <form action="Aula1/resposta.php" método="POST">
+        <input tipo="texto" nome="valor" />
+        <button type="submit">Enviar</button>
+    </form>
+
+</corpo>
+</html>
---
+++ resposta.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt">
+<cabeça>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="largura=largura-do-dispositivo, escala-inicial=1.0">
+    <title>Documento</title>
+</cabeçalho>
+<corpo>
+    <?php
+        $valor = $_POST[ 'valor' ];
+        echo  " Valor informado: $ valor " ;
+    ?>
+</corpo>
+</html>

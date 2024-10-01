532b9be44e2c349b290a3494e2bacfd50c518dae
jp-soaresr <soaresr0105@gmail.com>
2024-09-30 23:58:07 -0300

    fim

--- Lista_4/at_1/exer1.php
+++ Lista_4/at_1/exer1.php
@@ -1,70 +1,76 @@
-<!DOCTYPE html>
-<html lang="pt-br">
+<!doctype html>
+<html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <title>Cadastro de Contatos</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
-<body class="container">
-    <h1>Cadastro de Contatos</h1>
-    <form action="processar_contatos.php" method="POST">
-        <div id="contatos">
-            <div class="row mb-3 border p-3 rounded">
+<body class="container p-3">
+    <form action="" method="POST">
+        <?php for ($i = 1; $i <= 5; $i++): ?>
+            <div class="row">
                 <div class="col">
-                    <label for="nome1" class="form-label">Nome:</label>
-                    <input type="text" class="form-control" name="nome[]" id="nome1" required>
+                    <div class="mb-3">
+                        <label for="nomes" class="form-label">Informe o nome: </label>
+                        <input type="string" class="form-control" name="nomes[]">
+                    </div>
                 </div>
                 <div class="col">
-                    <label for="telefone1" class="form-label">Telefone:</label>
-                    <input type="text" class="form-control" name="telefone[]" id="telefone1" required>
-                </div>
-            </div>
-            <div class="row mb-3 border p-3 rounded">
-                <div class="col">
-                    <label for="nome2" class="form-label">Nome:</label>
-                    <input type="text" class="form-control" name="nome[]" id="nome2" required>
-                </div>
-                <div class="col">
-                    <label for="telefone2" class="form-label">Telefone:</label>
-                    <input type="text" class="form-control" name="telefone[]" id="telefone2" required>
+                    <div class="mb-3">
+                        <label for="telefones" class="form-label">Informe o numero de telefone desse usuario: </label>
+                        <input type="string" class="form-control" name="telefones[]">
+                    </div>
                 </div>
             </div>
-            <div class="row mb-3 border p-3 rounded">
-                <div class="col">
-                    <label for="nome3" class="form-label">Nome:</label>
-                    <input type="text" class="form-control" name="nome[]" id="nome3" required>
-                </div>
-                <div class="col">
-                    <label for="telefone3" class="form-label">Telefone:</label>
-                    <input type="text" class="form-control" name="telefone[]" id="telefone3" required>
-                </div>
-            </div>
-            <div class="row mb-3 border p-3 rounded">
-                <div class="col">
-                    <label for="nome4" class="form-label">Nome:</label>
-                    <input type="text" class="form-control" name="nome[]" id="nome4" required>
-                </div>
-                <div class="col">
-                    <label for="telefone4" class="form-label">Telefone:</label>
-                    <input type="text" class="form-control" name="telefone[]" id="telefone4" required>
-                </div>
-            </div>
-            <div class="row mb-3 border p-3 rounded">
-                <div class="col">
-                    <label for="nome5" class="form-label">Nome:</label>
-                    <input type="text" class="form-control" name="nome[]" id="nome5" required>
-                </div>
-                <div class="col">
-                    <label for="telefone5" class="form-label">Telefone:</label>
-                    <input type="text" class="form-control" name="telefone[]" id="telefone5" required>
-                </div>
+        <?php endfor; ?>
+
+        <div class="row">
+            <div class="col">
+                <button type="submit" class="btn btn-primary">Enviar</button>
             </div>
         </div>
-        <button type="submit" class="btn btn-primary">Cadastrar Contatos</button>
+
+        <?php
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            try {
+                $nomes = $_POST['nomes'];
+                $telefones = $_POST['telefones'];
+                $contatos = [];
+
+                $erro = false;
+
+                foreach ($nomes as $chave => $valor) {
+                    $telefone = $telefones[$chave];
+
+                    if (array_key_exists($valor, $contatos)) {
+                        echo "<p>O nome $valor já existe!</p>";
+                        $erro = true;
+                    } elseif (in_array($telefone, $contatos)) {
+                        echo "<p>O telefone $telefone já existe</p>";
+                        $erro = true;
+                    } else {
+                        $contatos[$valor] = $telefone;
+                    }
+                }
+
+                ksort($contatos);
+
+                if (!$erro) {
+                    echo "<p>Lista de contatos</p>";
+                    foreach ($contatos as $nome => $telefone) {
+                        echo "<p>Nome: $nome - Telefone: $telefone</p>";
+                    }
+                }
+            } catch (Exception $e) {
+                echo $e->getMessage();
+            }
+        }
+        ?>
     </form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
 </html>
